df72e1ae0499e822d646411418b51658a82b20b6
Nacho Fayos <nafama@alumnatflorida.es>
2025-11-04 11:19:36 +0100

    Resultado.php creado y boton para redirigir a index creado

---
+++ resultado.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+
+
+?>
+<!doctype html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<title>Mastermind Numérico</title>
+</head>
+<body>
+    <a href = 'index.php?reset=1'><button>Volver a jugar</button></a>
+</body>
+</html>
